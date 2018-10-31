7cef5fa6a3b0a471f0c88f07550e35286e668219
Alexey Dubrov <vhsw@users.noreply.github.com>
2018-10-31 22:40:27 +0300

    Update start.sh
    removed start wrong arguments

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ fi
 ln -sf /config/settings.json /var/lib/transmission-daemon/info/settings.json
 ln -sf /config/settings.json /etc/transmission-daemon/settings.json
 
-/usr/bin/transmission-daemon --foreground --config-dir /config --log-info --username ${USERNAME} --peerport 45555 --password ${PASSWORD} --auth --watch-dir /watch --download-dir /downloads --incomplete-dir /incomplete
+/usr/bin/transmission-daemon --foreground --config-dir /config --log-info
